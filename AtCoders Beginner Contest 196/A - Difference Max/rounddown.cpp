02a323518db73149a988354473413ab14fcb01c8
Omkar1114 <omkarjadhav1114@gmail.com>
2021-05-10 18:15:35 +0530

    B - round down

---
+++ AtCoders Beginner Contest 196/A - Difference Max/rounddown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::multiprecision;
+using namespace std;
+
+int main()
+{
+  cpp_int a;
+  cin >> a;
+  cout << a;
+  return 0;
+}
