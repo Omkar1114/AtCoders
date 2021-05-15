05de62e71689b74c56444038e5ccc3e3cbe35f16
Omkar1114 <omkarjadhav1114@gmail.com>
2021-05-15 20:46:00 +0530

    200_B

---
+++ KYOCERA Programming Contest 2021/B_200ABC_200.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  long long n, k;
+  cin >> n >> k;
+
+  for (int i = 0; i < k; i++)
+  {
+    if (n % 200 == 0)
+    {
+      n = n / 200;
+    }
+    else
+    {
+      n = n * 1000 + 200;
+    }
+  }
+
+  cout << n;
+  return 0;
+}
